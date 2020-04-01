1c7d82f728c2d0c2e0b58c83a6b20bce5a7cab3c
YanaShel <yana.shelementseva@gmail.com>
2020-04-01 11:15:16 +0300

    Refactor

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@
 
 const TASK_COUNT = 3;
 
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const taskListElement = siteMainElement.querySelector(`.board__tasks`);
+const boardElement = siteMainElement.querySelector(`.board`);
+
 const createSiteMenuTemplate = () => {
   return (
     `<section class="control__btn-wrap">
@@ -369,26 +374,15 @@ const createLoadMoreButtonTemplate = () => {
   );
 };
 
-const render = (container, template, place = `beforeend`) => {
-  container.insertAdjacentHTML(place, template);
+const render = (container, template, count = 1, place = `beforeend`) => {
+  for (let i = 0; i < count; i++) {
+    container.insertAdjacentHTML(place, template);
+  }
 };
-
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, createSiteMenuTemplate());
 render(siteMainElement, createFilterTemplate());
 render(siteMainElement, createBordTemplate());
-
-const taskListElement = siteMainElement.querySelector(`.board__tasks`);
-const boardElement = siteMainElement.querySelector(`.board`);
-
 render(taskListElement, createTaskEditTemplate());
-
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate());
-}
-
+render(taskListElement, createTaskTemplate(), TASK_COUNT);
 render(boardElement, createLoadMoreButtonTemplate());
-
-
