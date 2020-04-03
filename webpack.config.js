73b94645abfb6a8147b159d04345db73ac4fc331
YanaShel <yana.shelementseva@gmail.com>
2020-04-03 10:26:23 +0300

    install webpack and configure the build script

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main/js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `sourse-map`,
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    watchContentBase: true
+  }
+};
+
+
