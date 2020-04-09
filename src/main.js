04016e3ef70a8a90c3c0ec80ebcd338ecd088275
YanaShel <yana.shelementseva@gmail.com>
2020-04-09 08:56:50 +0300

    Creates a task data structure

--- src/main.js
+++ src/main.js
@@ -6,14 +6,19 @@ import {createTaskTemplate} from "./components/task";
 import {createTaskEditTemplate} from "./components/task-edit";
 import {createLoadMoreButtonTemplate} from "./components/load-more-button";
 import {generateFilters} from "./mock/filter";
+import {generateTasks} from "./mock/task";
 
 const TASK_COUNT_DEFAULT = 1;
-const TASK_COUNT = 3;
+const TASK_COUNT = 15;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
+let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
 const filters = generateFilters();
+const tasks = generateTasks(TASK_COUNT);
 
 const render = (container, template, count = TASK_COUNT_DEFAULT, place = `beforeend`) => {
   for (let i = 0; i < count; i++) {
@@ -29,6 +34,28 @@ const boardElement = siteMainElement.querySelector(`.board`);
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 
 render(boardElement, createSortingTemplate(), TASK_COUNT_DEFAULT, `afterbegin`);
-render(taskListElement, createTaskEditTemplate());
-render(taskListElement, createTaskTemplate(), TASK_COUNT);
+render(taskListElement, createTaskEditTemplate(tasks[0]));
 render(boardElement, createLoadMoreButtonTemplate());
+
+const renderPartOfTasks = (begin, end) => {
+  tasks.slice(begin, end)
+    .forEach((task) => render(taskListElement, createTaskTemplate(task)));
+};
+const addHandlerToLoadMoreButton = () => {
+  const loadMoreButton = boardElement.querySelector(`.load-more`);
+  loadMoreButton.addEventListener(`click`, () => {
+    const prevTasksCount = showingTasksCount;
+    showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+    renderPartOfTasks(prevTasksCount, showingTasksCount);
+
+    if (showingTasksCount >= tasks.length) {
+      loadMoreButton.remove();
+    }
+  });
+};
+
+renderPartOfTasks(1, showingTasksCount);
+addHandlerToLoadMoreButton();
+
+
