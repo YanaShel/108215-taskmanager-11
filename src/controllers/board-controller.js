f0b2ef641439c7751833536c1aebe9494a562444
YanaShel <yana.shelementseva@gmail.com>
2020-05-01 10:06:43 +0300

    Configures the board controller to work with the child task controller

--- src/controllers/board-controller.js
+++ src/controllers/board-controller.js
@@ -3,6 +3,7 @@ import NoTasks from "../components/board/no-tasks";
 import Tasks from "../components/board/task/tasks";
 import LoadMoreButton from "../components/board/load-more-button";
 import {remove, render} from "../util/dom-util";
+import TaskController from "./task-controller";
 
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
@@ -10,14 +11,29 @@ const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 export default class BoardController {
   constructor(container) {
     this._container = container;
+
+    this._tasks = [];
+    this._showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
     this._noTasks = new NoTasks();
     this._sort = new Sort();
     this._tasks = new Tasks();
     this._loadMoreButton = new LoadMoreButton();
   }
 
+  _renderTasks(taskList, tasks) {
+    return tasks.map((task) => {
+      const taskController = new TaskController(taskList);
+      taskController.render(task);
+
+      return taskController;
+    });
+  }
+
   render(tasks) {
+    this._tasks = tasks;
+
     const container = this._container.getElement();
+
     const isAllTasksArchived = tasks.every((task) => task.isArchive);
 
     if (isAllTasksArchived) {
@@ -30,20 +46,27 @@ export default class BoardController {
 
     const taskList = this._tasks.getElement();
 
-    let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
-    tasks.slice(0, showingTasksCount)
-      .forEach((task) => this._renderTask(taskList, task));
+    this._renderTasks(taskList, tasks.slice(0, this._showingTasksCount));
 
+    this._renderLoadMoreButton();
+  }
+
+  _renderLoadMoreButton() {
+    if (this._showingTasksCount >= this._tasks.length) {
+      return;
+    }
+
+    const container = this._container.getElement();
     render(container, this._loadMoreButton);
 
     this._loadMoreButton.setClickListener(() => {
-      const prevTasksCount = showingTasksCount;
-      showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+      const prevTasksCount = this._showingTasksCount;
+      const taskList = this._tasks.getElement();
+      this._showingTasksCount = this._showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
 
-      tasks.slice(prevTasksCount, showingTasksCount)
-        .forEach((task) => this._renderTask(taskList, task));
+      this._renderTasks(taskList, this._tasks.slice(prevTasksCount, this._showingTasksCount));
 
-      if (showingTasksCount >= tasks.length) {
+      if (this._showingTasksCount >= this._tasks.length) {
         remove(this._loadMoreButton);
       }
     });
